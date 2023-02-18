c0e83967c47f5ba58b03fd05c6e3d5d1851a4cd5
Leotrein <leoltrein@gmail.com>
2023-02-18 20:33:34 -0300

    initializing the board class

--- src/com/leot/model/Board.java
+++ src/com/leot/model/Board.java
@@ -1,5 +1,64 @@
 package com.leot.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+
+    private Integer line;
+    private Integer column;
+    private Integer mines;
+
+    private final List<Field> fields = new ArrayList<>();
+
+    public Board(Integer line, Integer column, Integer mines) {
+        this.line = line;
+        this.column = column;
+        this.mines = mines;
+
+        generateFields();
+        associateNeighbors();
+        spawnMines();
+    }
+
+    public Integer getLine() {
+        return line;
+    }
+
+    private void setLine(Integer line) {
+        this.line = line;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    private void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    public Integer getMines() {
+        return mines;
+    }
+
+    private void setMines(Integer mines) {
+        this.mines = mines;
+    }
+
+    public List<Field> getFields() {
+        return fields;
+    }
+
+    private void generateFields() {
+
+    }
+
+    private void associateNeighbors() {
+
+    }
+
+    private void spawnMines() {
+
+    }
 
 }
---
+++ test/com/leot/model/BoardTest.java
@@ -0,0 +1,11 @@
+package com.leot.model;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@Test
+	public void test() {
+		
+	}
+}
